b3219778118af47cc5b7a81c0e205f741fb867b2
andreigheorghiu-toast <andrei.gheorghiu@toasttab.com>
2023-03-06 04:40:50 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
-import { BuildOptions, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
-
-const getBuildOptions = (bundle = false): BuildOptions => ({
-  lib: {
-    entry: resolve(__dirname, "src/lib/index.ts"),
-    name: "ReactResponsiveness",
-    formats: ["es", "umd"],
-    fileName: `react-responsiveness${bundle ? ".bundle" : ""}`,
-  },
-  rollupOptions: {
-    external: ["react", "react-dom"].concat(bundle ? [] : ["jotai"]),
-    output: {
-      globals: Object.assign(
-        {},
-        {
-          react: "React",
-          "react-dom": "ReactDom",
-        },
-        bundle ? {} : { jotai: "Jotai" }
-      ),
-    },
-  },
-});
 
 export default defineConfig({
   plugins: [
@@ -38,7 +16,9 @@ export default defineConfig({
       name: "ReactResponsiveness",
       formats: ["es", "umd"],
       fileName: (format) =>
-        `react-responsiveness.${format}.${format === "umd" ? "c" : ""}js`,
+        `react-responsiveness.${format !== "es" ? format + "." : ""}${
+          format === "umd" ? "c" : ""
+        }js`,
     },
     rollupOptions: {
       external: ["react", "react-dom", "jotai"],
