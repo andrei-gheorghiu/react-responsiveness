5d110cdedf7a139a68f2cb9fd4c60ccf41ed10d5
andreigheorghiu-toast <andrei.gheorghiu@toasttab.com>
2023-03-06 05:50:12 +0000

    updates

--- src/lib/react-responsiveness.ts
+++ src/lib/react-responsiveness.ts
@@ -1,3 +1,4 @@
+import * as React from "react";
 import {
   ReactResponsivenessBreakpoints,
   ReactResponsivenessMatches,
@@ -67,8 +68,19 @@ export const useReactResponsiveness = () => {
       });
     });
   };
-  const isMin = (interval: string) => matches?.[interval]?.min;
-  const isMax = (interval: string) => matches?.[interval]?.max;
-  const isOnly = (interval: string) => matches?.[interval]?.only;
+
+  const isMin = React.useCallback(
+    (interval: string) => matches?.[interval]?.min,
+    [matches]
+  );
+  const isMax = React.useCallback(
+    (interval: string) => matches?.[interval]?.max,
+    [matches]
+  );
+  const isOnly = React.useCallback(
+    (interval: string) => matches?.[interval]?.only,
+    [matches]
+  );
+
   return { addListeners, isMin, isMax, isOnly, currentInterval, matches };
 };
